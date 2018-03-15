2bb0711e53e3ca333b4cc31f749689df34b783c3
Andrew Holguin <ajholguin1@gmail.com>
2018-03-15 15:04:57 -0700

    download zip code file

--- src/cal_water_service_areas.R
+++ src/cal_water_service_areas.R
@@ -27,3 +27,15 @@ if (!file.exists("data/water_service_areas/service_areas.shp")) {
   st_write(cal_water_sa_valid, "data/water_service_areas/service_areas_valid.shp", delete_layer = TRUE)
 }
 
+# CA Zip Codes -----------------------------------------------------------------
+
+if (!file.exists("data/census_zip/ca_zip.shp")) {
+  download.file("https://www2.census.gov/geo/tiger/TIGER2010/ZCTA5/2010/tl_2010_06_zcta510.zip",
+                "data/tl_2010_06_zcta510.zip")
+  unzip("data/tl_2010_06_zcta510.zip", exdir = "data/zip_codes")
+  
+  # re-project
+  ca_zip <- st_read("data/zip_codes/tl_2010_06_zcta510.shp") %>% st_transform(3310)
+  st_write(ca_zip, "data/zip_codes/ca_zip.shp", delete_layer = TRUE)
+}
+
